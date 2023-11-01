7951831c609d608a7608be200214a905766321f8
mari <eumarimonteiro45140@gmail.com>
2023-11-01 09:29:39 -0300

    att

---
+++ jQuery/index.js
@@ -0,0 +1,20 @@
+
+/* $("h1").addClass("big-tittle margin");
+
+$("h1").text("bye")
+$("button").html("<em>bye</em>")
+
+
+//Mudando a cor do clique do botao:
+
+
+$("button").click(function() {
+   $("h1").css("color", "purple")
+}
+)
+
+$("h1").on("click", function(){
+    $("h1").css("color", "purple")
+})
+
+*/
